4f455dcf6fd78495aed01b0e74630d3ef8a6ed00
lorenzoguerra01 <guerra.lo11-lg@gmail.com>
2024-04-29 16:30:29 +0200

    start base php

---
+++ js/script.js
@@ -0,0 +1,22 @@
+import {} from './utility.js';
+import {} from './data.js';
+
+const dt = luxon.DateTime;
+
+const {createApp} = Vue;
+createApp({
+    data(){
+        return{
+            message : 'Hello world!!!'
+        }
+    },
+    methods :{
+
+    },
+    computed: {
+        
+    },
+    mounted() {
+
+    }
+}).mount('#app');
